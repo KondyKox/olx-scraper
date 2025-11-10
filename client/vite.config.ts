3c9829a4f5d78e43c52ac822ed41483eee661dab
Konrad <kondykox@gmail.com>
2025-11-10 11:49:33 +0100

    vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: isProduction
+          ? "https://olx-scraper-api.onrender.com/"
+          : "http://localhost:5000",
         changeOrigin: true,
       },
     },
