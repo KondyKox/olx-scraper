76faebe6fee5a136aee4cc0a1393c7d3eff890d3
Konrad <kondykox@gmail.com>
2025-11-19 13:36:04 +0100

    average price

--- client/src/components/OffersList.tsx
+++ client/src/components/OffersList.tsx
@@ -5,11 +5,13 @@ import type { Offer } from "../types/OfferProps";
 import axios from "axios";
 import { useSavedOffer } from "../hooks/useSavedOffer";
 import { useSearch } from "../hooks/useSearch";
+import { useEffect, useState } from "react";
 
 const OffersList = () => {
   const { offers, loading } = useOffers();
   const { search } = useSearch();
   const { savedOffers, setSavedOffers } = useSavedOffer();
+  const [averagePrice, setAveragePrice] = useState<number>(0);
 
   const handleClick = async (e: React.MouseEvent, offer: Offer) => {
     e.stopPropagation();
@@ -28,46 +30,64 @@ const OffersList = () => {
     }
   };
 
-  return (
-    <ul className={styles.offersList}>
-      {loading ? (
-        <p>loading...</p> // zrobic skeletony zamiast tego
-      ) : !offers ? (
-        <h2 className={styles.noOffers}>Brak ofert 🥺🥺🥺</h2>
-      ) : (
-        offers.map((offer) => {
-          const isSaved = savedOffers.some((saved) => saved.id === offer.id);
+  useEffect(() => {
+    const sum = offers.reduce((acc, offer) => acc + offer.price, 0);
 
-          return (
-            <li key={offer.id} className={styles.offer}>
-              <a href={offer.url} target="_blank">
-                <img
-                  src={offer.image.src}
-                  alt={offer.image.alt}
-                  className={styles.offerImage}
-                />
-                <div className={styles.offerData}>
-                  <div className={styles.dataContainer}>
-                    <h3>{offer.title}</h3>
-                    <p className={styles.price}>{offer.price}zł</p>
-                  </div>
-                  <div className={styles.dataContainer}>
-                    <span>{offer.location}</span>
-                    <span>{offer.date}</span>
-                    <Heart
-                      className={`${styles.saveIcon} ${
-                        isSaved ? styles.saved : ""
-                      }`}
-                      onClick={(e) => handleClick(e, offer)}
-                    />
-                  </div>
-                </div>
-              </a>
-            </li>
-          );
-        })
+    if (offers.length > 0) {
+      const average = sum / offers.length;
+      const rounded = Number(average.toFixed(2));
+      setAveragePrice(rounded);
+    }
+  }, [offers]);
+
+  return (
+    <>
+      {offers && !loading && (
+        <h2>
+          Średnia cena:{" "}
+          <span className={styles.averagePrice}>{averagePrice}zł</span>
+        </h2>
       )}
-    </ul>
+      <ul className={styles.offersList}>
+        {loading ? (
+          <p>loading...</p> // zrobic skeletony zamiast tego
+        ) : !offers ? (
+          <h2 className={styles.noOffers}>Brak ofert 🥺🥺🥺</h2>
+        ) : (
+          offers.map((offer) => {
+            const isSaved = savedOffers.some((saved) => saved.id === offer.id);
+
+            return (
+              <li key={offer.id} className={styles.offer}>
+                <a href={offer.url} target="_blank">
+                  <img
+                    src={offer.image.src}
+                    alt={offer.image.alt}
+                    className={styles.offerImage}
+                  />
+                  <div className={styles.offerData}>
+                    <div className={styles.dataContainer}>
+                      <h3>{offer.title}</h3>
+                      <p className={styles.price}>{offer.price}zł</p>
+                    </div>
+                    <div className={styles.dataContainer}>
+                      <span>{offer.location}</span>
+                      <span>{offer.date}</span>
+                      <Heart
+                        className={`${styles.saveIcon} ${
+                          isSaved ? styles.saved : ""
+                        }`}
+                        onClick={(e) => handleClick(e, offer)}
+                      />
+                    </div>
+                  </div>
+                </a>
+              </li>
+            );
+          })
+        )}
+      </ul>
+    </>
   );
 };
 
